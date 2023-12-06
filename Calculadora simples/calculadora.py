804ada7c6b077a084907f9c446d96d95cd382201
Hernan Toro Junior <113436068+HernanJr0@users.noreply.github.com>
2023-12-06 17:37:47 -0400

    Add files via upload

---
+++ Calculadora simples/calculadora.py
@@ -0,0 +1,50 @@
+# Métodos das operações
+
+def soma(x, y):
+    return x + y
+
+def subtracao(x, y):
+    return x - y
+
+def multiplicacao(x, y):
+    return x * y
+
+def divisao(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return 'Erro: Divisão por zero!'
+    
+# Loop principal
+
+while True:
+    
+    # Opções para o usuário
+    print('\nEscolha uma operação')
+    print('1. Soma')
+    print('2. Subtração')
+    print('3. Multiplicação')
+    print('4. Divisão')
+    print('5. Sair')
+    
+    escolha = input("\nEscolha qual operação fazer (1-4), ou sair (5)\n")
+    
+    if escolha == '5':
+        print('Saindo da calculadora...')
+        break
+    elif escolha in ('1','2','3','4'):
+        num1 = float(input('Digite o primeiro número: '))
+        num2 = float(input('Digite o segundo número: '))
+        
+        if escolha == '1':
+            print('Resultado: ', soma(num1,num2))
+        elif escolha == '2':
+            print('Resultado: ', subtracao(num1,num2))
+        elif escolha == '3':
+            print('Resultado: ', multiplicacao(num1,num2))
+        elif escolha == '4':
+            print('Resultado: ', divisao(num1,num2))
+        else:
+            print('Opção inválida. Tente novamente.')
+    else:
+        print('Opção inválida. Tente novamente.')
